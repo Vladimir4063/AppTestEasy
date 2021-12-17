ba59fa931079dfcadbc9d397625106afed674451
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-12-17 14:37:07 -0300

    CopiaPortaPapeles terminado

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+
+const elemento = document.querySelector('.contenido-texto')
+
+document.querySelector('#btn-copy').addEventListener('click',()=>{
+    // mensaje copy
+    document.querySelector('.mensaje-copy').classList.add('show');
+    
+    copytoClickBoard(elemento)
+
+    setTimeout(()=>{
+        document.querySelector('.mensaje-copy').classList.remove('show');
+    },1300)
+})
+
+function copytoClickBoard(elemento){
+    const inputOculto = document.createElement('input')
+
+    // envio valor/contenido
+    inputOculto.setAttribute('value', elemento?.innerText)
+
+    document.body.appendChild(inputOculto)
+
+    inputOculto.select()
+    navigator.clipboard.writeText(inputOculto.value)
+    // document.execCommand('copy');
+
+    document.body.removeChild(inputOculto)
+}
