523aa7767a3be66424f81a044de1d764d4dd4d72
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-12-28 22:27:20 -0300

    Se entrego proyecto a Sil

---
+++ js/main-en.js
@@ -0,0 +1,31 @@
+
+
+const elemento = document.querySelector('.contenido-texto')
+
+document.querySelector('#btn-copy').addEventListener('click',()=>{
+    // mensaje copy
+    
+    // document.querySelector('.mensaje-copy').classList.add('show');
+    
+    copytoClickBoard(elemento)
+
+    alert("¡Copied to clipboard!")
+
+    // setTimeout(()=>{
+    //     document.querySelector('.mensaje-copy').classList.remove('show');
+    // },1300)
+})
+
+function copytoClickBoard(elemento){
+    const inputOculto = document.createElement('input')
+
+    // envio valor/contenido
+    inputOculto.setAttribute('value', elemento?.innerText)
+
+    document.body.appendChild(inputOculto)
+
+    inputOculto.select()
+    navigator.clipboard.writeText(inputOculto.value)
+
+    document.body.removeChild(inputOculto)
+}
